0f7acd3cb6e79c9179af3511c02cc4034e30cb4b
pixsysedp <edp@pixsys.net>
2022-04-07 16:07:07 +0200

    Commit per cambio server.

--- scripts/codesys.sh
+++ scripts/codesys.sh
@@ -15,22 +15,22 @@ if [ $status == 0 ]; then
 else
   echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 fi
-systemctl restart ssh
+#systemctl restart ssh
 
 # aggiungi i gruppi "i2c" e "spi"
 getent group i2c >/dev/null || addgroup --system i2c
 getent group spi >/dev/null || addgroup --system spi
 
 # installa il runtime di codesys
-ln -s ../../data/codesys /var/opt/codesys
+#ln -s ../../data/codesys /var/opt/codesys
 apt install -y /root/codesyscontrol*.deb
 rm /root/codesyscontrol*.deb
 apt clean
-chgrp -R plc /data/codesys
-chmod -R g+rwxs /data/codesys
+#chgrp -R plc /data/codesys
+#chmod -R g+rwxs /data/codesys
 
 # disabilita il servizio (sulla eMMC viene attivato in /etc/rc.firstboot)
-systemctl disable codesyscontrol.service
+#systemctl disable codesyscontrol.service
 
 # configurazione CodeSys
 perl -i -pe 's/^(Command.0=shutdown)$/$1\nCommand=AllowAll/' /etc/CODESYSControl.cfg
